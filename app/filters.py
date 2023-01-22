4f419b3bad24914629f64ed4bb996c059b2e6114
abdoohossamm <abdoohossamm@outlook.com>
2023-01-22 15:06:16 +0200

    Add branch to session's filter

--- app/filters.py
+++ app/filters.py
@@ -1,6 +1,6 @@
 from psycopg2 import Date
 import django_filters #type: ignore
-from app.models import Session, Teacher
+from app.models import Session, Teacher, Branch
 from django import forms
 from django_filters import DateFilter, CharFilter, ModelChoiceFilter # type: ignore
 from django.conf import settings
@@ -8,12 +8,16 @@ from .forms import TimeInput
 
 class SessionFilter(django_filters.FilterSet):
     time = CharFilter(label='الميعاد', field_name='time', widget=TimeInput())
+    branch = django_filters.ModelChoiceFilter(queryset=Branch.objects.all(), field_name="name__branch")
+
     class Meta:
         model = Session
         fields = '__all__'
+
     def __init__(self, *args, **kwargs):
         super(SessionFilter, self).__init__(*args, **kwargs)
         self.filters['day'].label="يوم"
         self.filters['teacher'].label="اسم المحفظ"
         self.filters['time'].label="الميعاد"
         self.filters['name'].label="الغرفة"
+        self.filters['branch'].label = "الفرع"
